552d31444a16497c12443dce507b1740f30dc5d3
Thes06 <111084458+Thes06@users.noreply.github.com>
2022-08-12 10:10:28 +0200

    Add files via upload

---
+++ agrlib.py
@@ -0,0 +1,9 @@
+def salsapom(a):
+  b = (a/100)*75
+  print("La salsa ottunuta da "+str(a)+"kg di pomodori è di "+str(b)+"L")
+def olioevo(a):
+  b = (a/100)*15
+  print("Olio evo ottunuto da "+str(a)+"kg di olive è di "+str(b)+"L")
+def vino(a):
+  b = (a/100)*50
+  print("Vino ottunuto da "+str(a)+"kg di vino è di "+str(b)+"L")
